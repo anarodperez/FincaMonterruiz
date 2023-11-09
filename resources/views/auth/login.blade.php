018300f409daa95a7d6ebf92543e2784c2ce27f4
Anarp <anamaria.rodriguez.iesdonana.org>
2023-11-09 18:48:17 +0100

    pequeños cambios en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -77,7 +77,7 @@
                         @if (Route::has('password.request'))
                             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 href="{{ route('password.request') }}">
-                                {{ __('¿Has olvidado la contraseña?') }}
+                                {{ __('¿Has olvidado la contraseña? Haz click en este enlace.') }}
                             </a>
                         @endif
                     </div>
