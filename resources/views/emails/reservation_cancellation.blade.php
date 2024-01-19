1e75381090702efccdf3797089a93c7f51f602c6
Anarp <anamaria.rodriguez.iesdonana.org>
2024-01-19 11:48:13 +0100

    correccion mensaje correo

--- resources/views/emails/reservation_cancellation.blade.php
+++ resources/views/emails/reservation_cancellation.blade.php
@@ -34,10 +34,8 @@
     <div class="container">
         <h1>Reserva Cancelada</h1>
         <p>Hola {{ $reserva->usuario->nombre }},</p>
-
-         <p>Hemos procesado un reembolso a tu cuenta de PayPal. Por favor, verifica tu cuenta de PayPal para confirmar la devolución.</p>
-
         <p>Lamentamos informarte que tu reserva para la actividad <strong>{{ $reserva->actividad->nombre }}</strong> programada para el <strong>{{ $reserva->horario->fecha }} a las {{ $reserva->horario->hora }}</strong> ha sido cancelada.</p>
+        <p>Hemos procesado un reembolso a tu cuenta de PayPal. Por favor, verifica tu cuenta de PayPal para confirmar la devolución.</p>
         <p>Si tienes alguna consulta o deseas reprogramar tu reserva, no dudes en contactarnos.</p>
         <p class="footer">Gracias por elegirnos,<br>El equipo de {{ config('app.name') }}</p>
     </div>
